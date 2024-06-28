1424dba18c543e9431dae41ee75683a0d2f11055
mufidu <mufid.to@gmail.com>
2024-06-28 19:08:30 +0700

    Implement get cart and remove book from cart for user

--- pkg/api/user/cart.go
+++ pkg/api/user/cart.go
@@ -8,10 +8,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// Add to Cart: Customers can add books to their shopping cart.
-// Update Cart: Customers can update the quantity or remove items from their cart.
-// Total Price Calculation: The total price of items in the cart is automatically calculated each time the cart is updated.
-
 // @BasePath /api/v1
 
 // AddToCart godoc
@@ -22,7 +18,6 @@ import (
 // @Accept  json
 // @Produce  json
 // @Security BearerAuth
-// Should ask for the book id and the quantity in body, not the cartbook
 // @Param   id     body    int     false        "Book ID"
 // @Param   quantity     body    int     false        "Quantity"
 // @Success 200 {object} models.Cart	"Successfully added book to cart"
@@ -45,13 +40,16 @@ func AddToCart(c *gin.Context) {
 		return
 	}
 
+	// Fetch the user
+	user := models.User{}
+	if err := database.DB.Where("username = ?", username).First(&user).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+
 	// Fetch the cart of the user, if it doesn't exist, create it
-	if err := database.DB.Where("user_id = ?", username).First(&cart).Error; err != nil {
-		user := models.User{}
-		if err := database.DB.Where("username = ?", username).First(&user).Error; err != nil {
-			c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
-			return
-		}
+	if err := database.DB.Where("user_id = ?", user.ID).Preload("Books").Preload("User").First(&cart).Error; err != nil {
+		cart.User = user
 		cart.UserID = user.ID
 		database.DB.Create(&cart)
 	}
@@ -81,4 +79,107 @@ func AddToCart(c *gin.Context) {
 
 	// Send the updated cart and the cartbook
 	c.JSON(http.StatusOK, gin.H{"cart": cart, "cartbook": cartBook})
+}
+
+// RemoveFromCart godoc
+// @Summary Remove a book from the cart
+// @Schemes http
+// @Description Remove a book from the cart of the currently logged in user
+// @Tags user
+// @Accept  json
+// @Produce  json
+// @Security BearerAuth
+// @Param   id     path    int     true        "Book ID"
+// @Success 200 {object} models.Cart	"Successfully removed book from cart"
+// @Failure 400 {string} string "Bad Request"
+// @Failure 401 {string} string "Unauthorized"
+// @Failure 404 {string} string "Book not found"
+// @Failure 500 {string} string "Internal Server Error"
+// @Router /user/cart/{id} [delete]
+func RemoveFromCart(c *gin.Context) {
+	var cart models.Cart
+	var book models.Book
+	var cartBook models.CartBook
+
+	// Middleware already sets the username in the context
+	username := c.GetString("username")
+
+	// Get the user id
+	user := models.User{}
+	if err := database.DB.Where("username = ?", username).First(&user).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+
+	// Fetch the cart of the user
+	if err := database.DB.Where("user_id = ?", user.ID).Preload("Books").Preload("User").First(&cart).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Cart not found"})
+		return
+	}
+
+	// Fetch the book
+	if err := database.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Book not found"})
+		return
+	}
+
+	// Fetch the cartbook
+	if err := database.DB.Where("book_id = ? AND cart_id = ?", book.ID, cart.ID).First(&cartBook).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Book not in cart"})
+		return
+	}
+
+	// Calculate the total price
+	cart.TotalPrice -= book.Price * cartBook.Quantity
+	database.DB.Save(&cart)
+
+	// Remove the book from the cart
+	database.DB.Where("book_id = ? AND cart_id = ?", book.ID, cart.ID).Delete(&cartBook)
+
+	// If cartbook still exists, throw an error
+	if database.DB.Where("book_id = ? AND cart_id = ?", book.ID, cart.ID).First(&cartBook).Error == nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to remove book from cart"})
+		return
+	}
+
+	// Send the updated cart
+	c.JSON(http.StatusOK, gin.H{"Book removed from cart": book})
+}
+
+// GetCart godoc
+// @Summary Get the cart
+// @Schemes http
+// @Description Get the cart of the currently logged in user
+// @Tags user
+// @Accept  json
+// @Produce  json
+// @Security BearerAuth
+// @Success 200 {object} models.Cart	"Successfully fetched cart"
+// @Failure 400 {string} string "Bad Request"
+// @Failure 401 {string} string "Unauthorized"
+// @Failure 404 {string} string "Cart not found"
+// @Failure 500 {string} string "Internal Server Error"
+// @Router /user/cart [get]
+func GetCart(c *gin.Context) {
+	var cart models.Cart
+
+	// Middleware already sets the username in the context
+	username := c.GetString("username")
+
+	// Fetch the user
+	user := models.User{}
+	if err := database.DB.Where("username = ?", username).First(&user).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+
+	// Fetch the cart of the user, if it doesn't exist, create it
+	if err := database.DB.Where("user_id = ?", user.ID).Preload("Books").Preload("User").First(&cart).Error; err != nil {
+		cart.User = user
+		cart.UserID = user.ID
+		database.DB.Create(&cart)
+	}
+
+	// Send the cart
+	c.JSON(http.StatusOK, gin.H{"cart": cart})
 }
